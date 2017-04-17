42fbdeb7f88296a6bee4a3de8ddea5d1fa3ad471
hmf <zxc723455362@qq.com>
2017-04-17 16:24:53 +0800

    css3动画效果

---
+++ css3animation/index.js
@@ -0,0 +1,347 @@
+$(function() {
+	$('#css1').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation1');
+		return false;
+	});
+	$('#css2').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation2');
+		return false;
+	});
+	$('#css3').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation3');
+		return false;
+	});
+	$('#css4').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation4');
+		return false;
+	});
+	$('#css5').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation5');
+		return false;
+	});
+	$('#css6').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation6');
+		return false;
+	});
+	$('#css7').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation7');
+		return false;
+	});
+	$('#css8').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation8');
+		return false;
+	});
+	$('#css9').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation9');
+		return false;
+	});
+	$('#css10').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation10');
+		return false;
+	});
+	$('#css11').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation11');
+		return false;
+	});
+	$('#css12').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation12');
+		return false;
+	});
+	$('#css13').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation13');
+		return false;
+	});
+	$('#css14').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation14');
+		return false;
+	});
+	$('#css15').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation15');
+		return false;
+	});
+	$('#css16').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation16');
+		return false;
+	});
+	$('#css17').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation17');
+		return false;
+	});
+	$('#css18').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation18');
+		return false;
+	});
+	$('#css19').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation19');
+		return false;
+	});
+	$('#css20').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation20');
+		return false;
+	});
+	$('#css21').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation21');
+		return false;
+	});
+	$('#css22').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation22');
+		return false;
+	});
+	$('#css23').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation23');
+		return false;
+	});
+	$('#css24').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation24');
+		return false;
+	});
+	$('#css25').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation25');
+		return false;
+	});
+	$('#css26').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation26');
+		return false;
+	});
+	$('#css27').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation27');
+		return false;
+	});
+	$('#css28').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation28');
+		return false;
+	});
+	$('#css29').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation29');
+		return false;
+	});
+	$('#css30').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation30');
+		return false;
+	});
+	$('#css31').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation31');
+		return false;
+	});
+	$('#css32').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation32');
+		return false;
+	});
+	$('#css33').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation33');
+		return false;
+	});
+	$('#css34').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation34');
+		return false;
+	});
+	$('#css35').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation35');
+		return false;
+	});
+	$('#css36').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation36');
+		return false;
+	});
+	$('#css37').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation37');
+		return false;
+	});
+	$('#css38').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation38');
+		return false;
+	});
+	$('#css39').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation39');
+		return false;
+	});
+	$('#css40').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation40');
+		return false;
+	});
+	$('#css41').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation41');
+		return false;
+	});
+	$('#css42').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation42');
+		return false;
+	});
+	$('#css43').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation43');
+		return false;
+	});
+	$('#css44').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation44');
+		return false;
+	});
+	$('#css45').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation45');
+		return false;
+	});
+	$('#css46').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation46');
+		return false;
+	});
+	$('#css47').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation47');
+		return false;
+	});
+	$('#css48').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation48');
+		return false;
+	});
+	$('#css49').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation49');
+		return false;
+	});
+	$('#css50').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation50');
+		return false;
+	});
+	$('#css51').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('animation51');
+		return false;
+	});
+	$('#css52').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('slideDown');
+		return false;
+	});
+	$('#css53').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('slideUp');
+		return false;
+	});
+	$('#css54').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('slideLeft');
+		return false;
+	});
+	$('#css55').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('slideRight');
+		return false;
+	});
+	$('#css56').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('slideExpandUp');
+		return false;
+	});
+	$('#css57').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('expandUp');
+		return false;
+	});
+	$('#css58').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('bounce');
+		return false;
+	});
+	$('#css59').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('pulse');
+		return false;
+	});
+	$('#css60').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('bigEntrance');
+		return false;
+	});
+	$('#css61').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('expandOpen');
+		return false;
+	});
+	$('#css62').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('fadeIn');
+		return false;
+	});
+	$('#css63').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('hatch');
+		return false;
+	});
+	$('#css64').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('floating');
+		return false;
+	});
+	$('#css65').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('tossing');
+		return false;
+	});
+	$('#css66').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('pullUp');
+		return false;
+	});
+	$('#css67').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('pullDown');
+		return false;
+	});
+	$('#css68').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('stretchLeft');
+		return false;
+	});
+	$('#css69').click(function() {
+		$('#right').removeClass();
+		$('#right').addClass('stretchRight');
+		return false;
+	});
+})
