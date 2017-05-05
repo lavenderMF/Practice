5ac8ed6a32a15b8ad1a4177fd90f7be9414c2c8e
hmf <zxc723455362@qq.com>
2017-05-05 18:07:45 +0800

    仿网易云音乐项目练手创建

---
+++ wyymusic/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+  	<v-header></v-header>
+  </div>
+</template>
+
+<script>
+import header from './components/header';
+
+export default {
+  name: 'app',
+  components: {
+  	'v-header': header
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ wyymusic/src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="header">
+		<div class="get-user"></div>
+		<div class="menu-tab"></div>
+		<div class="search-btn"></div>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style>
+.header{
+	height: 50px;
+	background: #c62f2f;
+	display: flex;
+}
+.get-user{
+	flex: 0 0 50px;
+	background: #7E8C8D;
+}
+.menu-tab{
+	flex: 1;
+}
+.search-btn{
+	flex: 0 0 50px;
+	background: #42B983;
+}
+</style>
